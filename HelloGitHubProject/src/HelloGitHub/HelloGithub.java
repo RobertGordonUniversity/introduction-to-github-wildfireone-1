dfe25ee1f1c0d8d131725582bd88c19cde037cab
access02 <access02@RG-N533-POD.rgu.ac.uk>
2019-02-04 16:18:36 +0000

    added an extra printlin

--- HelloGitHubProject/src/HelloGitHub/HelloGithub.java
+++ HelloGitHubProject/src/HelloGitHub/HelloGithub.java
@@ -5,7 +5,7 @@ public class HelloGithub {
     public static void main(String[] args) {
         // Prints "Hello, Github" to the terminal window.
         System.out.println("Hello, Github");
-        System.out.println("Hello to you too");
+        System.out.println("Hello to you aswell");
         
     }
 
