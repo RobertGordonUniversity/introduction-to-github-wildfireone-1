f21566ebcda9c16c4f235c624ce12fa4bd636a00
John Isaacs <wildfireone@gmail.com>
2019-02-04 12:29:26 +0000

    Delete hellogithub.java

--- hellogithub.java
+++
@@ -1,8 +0,0 @@
-public class HelloGithub {
-
-    public static void main(String[] args) {
-        // Prints "Hello, Github" to the terminal window.
-        System.out.println("Hello, Github");
-    }
-
-}
