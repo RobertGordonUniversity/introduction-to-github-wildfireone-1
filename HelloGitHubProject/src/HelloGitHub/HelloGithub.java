77d755147c0a7471e5f2ce1c2e8b4bf037852c3c
John Isaacs <wildfireone@gmail.com>
2019-02-04 12:34:47 +0000

    Initial config

---
+++ HelloGitHubProject/src/HelloGitHub/HelloGithub.java
@@ -0,0 +1,10 @@
+package HelloGitHub;
+
+public class HelloGithub {
+
+    public static void main(String[] args) {
+        // Prints "Hello, Github" to the terminal window.
+        System.out.println("Hello, Github");
+    }
+
+}
