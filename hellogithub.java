046e2d6ba6f27c0ab27f66ec4ddc2af30f1e44a4
John Isaacs <wildfireone@gmail.com>
2019-02-04 11:53:53 +0000

    Create hellogithub.java

---
+++ hellogithub.java
@@ -0,0 +1,8 @@
+public class HelloGithub {
+
+    public static void main(String[] args) {
+        // Prints "Hello, Github" to the terminal window.
+        System.out.println("Hello, Github");
+    }
+
+}
